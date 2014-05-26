37eda3bd9e5e5265ae63e44a9d618561fe75d1d5
Sebastian Nemak <der-basti@users.noreply.github.com>
2014-05-26 15:35:16 +0200

    issue #19, update samples

---
+++ sourcecode/Hello.java
@@ -0,0 +1,19 @@
+package de.th.wildau;
+
+/**
+ * Demo java class.
+ * 
+ * @author s7n
+ */
+public class Hello {
+
+	/**
+	 * Run method.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
